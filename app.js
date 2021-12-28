4b4c21bb624d8b910d0687bd3c6759b73e634bef
d1bolozan <d1bolozan@gmail.com>
2021-12-28 16:09:37 +0200

    ADD : CRUD for items using localStorage

--- app.js
+++ app.js
@@ -1,40 +1,117 @@
-// Save previous theme in LocalStorage
-let darkMode = localStorage.getItem("darkMode");
-const themeBtn = document.getElementById("themeBtn");
+const form = document.querySelector(".form");
+const newTodo = document.getElementById("newTodo");
+const todoList = document.querySelector(".list");
+const amountItems = document.getElementById("amount");
+const filter = document.querySelector(".filter");
+const clearButton = document.getElementById("clear-btn");
+let todoItems = [];
 
-console.log(darkMode);
-// check if dark mode is enable
-// if it's enabled, turn it off
-// if it's disabled, turn it on
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-const enableDarkMode = () => {
-  // add the class darkmode to the body
-  document.body.classList.add("darkmode");
-  if (themeBtn.classList.contains("icon-moon")) {
-    themeBtn.classList.remove("icon-moon");
-    themeBtn.classList.add("icon-sun");
+  if (newTodo.value.length > 0) addTodoItem(newTodo.value);
+  form.reset();
+});
+
+const addTodoItem = (item) => {
+  if (item !== "") {
+    todoItems.push({
+      id: Date.now(),
+      text: item,
+      status: false,
+    });
+    addItemInLocalStorage(todoItems);
   }
-  // update darkMode in the localStorage
-  localStorage.setItem("darkMode", "enabled");
 };
 
-if (darkMode === "enabled") {
-    enableDarkMode();
-  }
-const disableDarkMode = () => {
-  document.body.classList.remove("darkmode");
-  if (themeBtn.classList.contains("icon-sun")) {
-    themeBtn.classList.remove("icon-sun");
-    themeBtn.classList.add("icon-moon");
-  }
-  localStorage.setItem("darkMode", null);
+const showTodoItems = (array) => {
+  todoList.innerHTML = "";
+  array.map((item) => {
+    const todoWrapper = document.createElement("li");
+    todoWrapper.classList.add("list__item");
+    todoWrapper.setAttribute("data-id", item.id);
+    todoWrapper.innerHTML = `
+      <label class="list__label">
+        <input type="checkbox" class="list__checkbox list__checkbox-icon" ${item.status === true ? "checked" : ""} />
+        <span class="list__checkbox--fake"><span class="list__checkbox-icon icon-check"></span></span>
+        <span class="list__text">${item.text}</span>
+        <button class="list__remove icon-cross"></button>
+      </label>`;
+    todoList.append(todoWrapper);
+    showCountOfItems(array);
+  });
 };
 
-themeBtn.addEventListener("click", () => {
-  darkMode = localStorage.getItem("darkMode");
-  if (darkMode !== "enabled") {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
+const addItemInLocalStorage = (items) => {
+  localStorage.setItem("todoItems", JSON.stringify(items));
+
+  showTodoItems(items);
+};
+
+const getItemsFromLocalStorage = () => {
+  const temp = localStorage.getItem("todoItems");
+
+  if (temp) {
+    todoItems = JSON.parse(temp);
+    showTodoItems(todoItems);
   }
+};
+
+const showCountOfItems = (array) => {
+  amountItems.innerText = array.length;
+};
+
+const updateStatusOfItem = (id) => {
+  todoItems.map((item) => {
+    if (item.id == id) {
+      item.status = !item.status;
+    }
+  });
+  addItemInLocalStorage(todoItems);
+}
+
+const deleteItem = (id) => {
+  todoItems = todoItems.filter((item) => item.id != id);
+  addItemInLocalStorage(todoItems);
+};
+
+getItemsFromLocalStorage();
+
+todoList.addEventListener("click", (event) => {
+  if (event.target.type === "checkbox") {
+    updateStatusOfItem(event.target.parentElement.parentElement.getAttribute("data-id"));
+  }
+  if (event.target.type === "submit") {
+    deleteItem(event.target.parentElement.parentElement.getAttribute("data-id"));
+  }
+});
+
+const showItemsByCondition = (options) => {
+  let temp = [];
+  switch (options) {
+    case "completed":
+      temp = todoItems.filter((item) => item.status === true);
+      showTodoItems(temp);
+      break;
+    case "active":
+      temp = todoItems.filter((item) => item.status === false);
+      console.log(temp);
+      showTodoItems(temp);
+      break;
+    default:
+      showTodoItems(todoItems);
+      break;
+  }
+};
+
+
+filter.addEventListener("click", (event) => {
+  if (event.target.id !== "") {
+    const options = event.target.id;
+    showItemsByCondition(options);
+  }
+});
+
+clearButton.addEventListener("click", () => {
+  todoItems.map((elem) => (elem.status === true ? deleteItem(elem.id) : ""));
 });
